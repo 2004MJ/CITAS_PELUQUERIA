2df9f1f176040f85c6013f33e6a2582078fd91cf
youssef <youssefjabri232@gmail.com>
2025-12-02 18:10:51 +0100

    SALAH

--- app/src/main/java/com/example/citas_peluqueria/Login.kt
+++ app/src/main/java/com/example/citas_peluqueria/Login.kt
@@ -6,15 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SALAH : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_salah)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }  //weee hania
+    }
+
+
 }
